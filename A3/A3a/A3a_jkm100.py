7508989b79c99dc57e14e9ebb9d588418a6c7869
John Mays <25927827+johnmays@users.noreply.github.com>
2022-02-23 23:56:09 -0500

    almost finished with A3a; couldnt finish quiz in time.  maybe that's a good thing.

---
+++ A3/A3a/A3a_jkm100.py
@@ -0,0 +1,143 @@
+# Some important imports
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+# importing A1b_jkm100.py purely for function default kwargs
+import sys
+sys.path.insert(1, '/Users/johnmays/Documents/GitHub/CSDS_364_HW/A1')
+from A1b_jkm100 import *
+
+purples = ["#0a0612", "#392249", "#482980", "#673ab7",
+           "#7a52aa", "#9779bd", "#b59fd0", "#d3c5e3"]
+
+def plot_sampled_function(g=sinewave, fs=1, f=1.0, tlim=(0, 2*math.pi), tscale=1, tunits="secs"):
+    # all values are given in seconds; the time scale will alter the plot to show x-axis values on the order of the tscale
+    t = np.linspace(tlim[0], tlim[1], 2001)
+    signal = g(t=t, f=f)
+    sampled_t = np.linspace(tlim[0], tlim[1], int(((tlim[1]-tlim[0])*fs)+1))
+    sampled_signal = g(t=sampled_t, f=f)
+    plt.figure(figsize=(8, 5), dpi=72)
+    plt.plot(t*tscale, signal, c=purples[6], label="signal")
+    markers, stems, base = plt.stem(sampled_t*tscale, sampled_signal, linefmt=purples[2], basefmt=" ", label="sampled signal")
+    plt.setp(markers, 'color', purples[2])
+    plt.setp(markers, 'marker', 'o')
+    plt.xlim(tlim[0]*tscale, tlim[1]*tscale)
+    plt.xlabel("Time ({units})".format(units=tunits))
+    plt.ylabel("Amplitude")
+    plt.title("Sampled Signal with Original Signal")
+    plt.legend()
+    plt.show()
+    return None
+
+def delta(t, fs=1):
+    center_time_val = 0
+    if type(t) == np.ndarray or type(t) == list:
+        signal = []
+        for time_val in t:
+            if (time_val >= center_time_val - 1/(2*fs)) and (time_val < center_time_val + 1/(2*fs)):
+                signal.append(1)
+            else:
+                signal.append(0)
+        return signal
+    else:  # t must be a number then
+        if (t >= center_time_val - 1/(2*fs)) and (t <= center_time_val + 1/(2*fs)):
+            return 1
+        else:
+            return 0
+
+def u(t):
+    if type(t) == np.ndarray or type(t) == list:
+        signal = []
+        for time_val in t:
+            if time_val >= 0:
+                signal.append(1)
+            else:
+                signal.append(0)
+        return signal
+    else:  # t must be a number then
+        if t >= 0:
+            return 1
+        else:
+            return 0
+
+def gensignal(t, g, tau=0.025, T=0.1):
+    signal = g(t-tau)
+    for i in range(len(signal)):
+        if t[i] < tau or t[i] >= tau + T:
+            signal[i] = 0
+    return np.array(signal)
+
+def energy(x):
+    return np.linalg.norm(x, ord=2)**2
+
+def power(x):
+    return energy(x)/len(x)
+
+def snr(Ps, Pn):
+    return 10*np.log10(Ps/Pn)
+
+def noisysignal(t, g, tau=0.025, T=0.1, sigma=1):
+    if type(t) == np.ndarray or type(t) == list: # for t as an array
+        signal = []
+        for t_val in t:
+            noise_val = np.random.normal(loc=0, scale=sigma)
+            if t_val < tau or t_val >= tau + T:
+                signal.append(noise_val)
+            else:
+                signal.append(noise_val + g(t_val))
+        return signal
+    else: # for single time values t
+        noise_val = np.random.normal(loc=0, scale=sigma)
+        if t < tau or t >= tau + T:
+            return noise_val
+        else:
+            return noise_val + g(t)
+
+def plotsignal(g=lambda t: noisysignal(1, g = lambda t: u(t)), tlim=(0, 2*math.pi), tscale=1, tunits="secs", title="Signal", show_sampled=False, fs=100):
+    # g should be an anonymized function
+    t = np.linspace(tlim[0], tlim[1], 2001)
+    t = t
+    signal = []
+    for t_val in t:
+        signal.append(g(t_val))
+    plt.figure(figsize=(8, 5), dpi=72)
+    plt.plot(t*tscale, signal, c=purples[6], label="Signal with Noise")
+    plt.xlim(tlim[0]*tscale, tlim[1]*tscale)
+    plt.xlabel("Time ({units})".format(units=tunits))
+    plt.ylabel("Amplitude")
+    plt.title(title)
+    if show_sampled:
+        sampled_t = np.linspace(tlim[0], tlim[1], int(((tlim[1]-tlim[0])*fs)+1))
+        sampled_signal = []
+        for sampled_t_val in sampled_t:
+            sampled_signal.append(g(sampled_t_val))
+        markers, stems, base = plt.stem(sampled_t*tscale, sampled_signal, linefmt=purples[2], basefmt=" ", label="sampled signal")
+        plt.setp(markers, 'color', purples[2])
+        plt.setp(markers, 'marker', 'o')
+        plt.legend()
+    plt.show()
+    return None
+
+def snr2sigma(x, xrange=None, snr=10):
+    if xrange == None:
+        xrange = (0, len(x))
+    Ps = power(x[xrange[0]:xrange[1]])
+    sigma_s = math.sqrt(Ps)
+    sigma_n = sigma_s/(10**(snr/20))
+    return sigma_n
+
+def extent(y, theta=0.01):
+    max_abs_value = 0
+    for y_val in y:
+        if np.abs(y_val) > max_abs_value:
+            max_abs_value = np.abs(y_val)
+    threshold_value = theta*max_abs_value
+    first_thresh_index = None
+    last_thresh_index = None
+    for i in range(len(y)):
+        if np.abs(y[i]) > threshold_value:
+            if first_thresh_index == None:
+                first_thresh_index = i
+            last_thresh_index = i
+    return first_thresh_index, last_thresh_index    
---
+++ A3/A3a/Submissions/464-A3a_jkm100_files/A1b_jkm100.py
@@ -0,0 +1,222 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+import wave  # for importing .wav files as iterables
+
+# 1a
+
+
+def sinewave(t, f=1.0, d=0.0):
+    if(type(t) == list):
+        t = np.array(t)
+    phi = 2 * np.pi * d * f
+    return np.sin(2 * np.pi * f * t + phi)
+
+def cosinewave(t, f=1.0, d=0.0):
+    if(type(t) == list):
+        t = np.array(t)
+    phi = 2 * np.pi * d * f
+    return np.cos(2 * np.pi * f * t + phi)
+
+
+def plot_sinewave(t, f=4.5, d=1.0):
+    figure(figsize=(8, 6), dpi=80)
+    sine_vals = sinewave(t, f=4.5, d=1.0)
+    plt.plot(t, sine_vals)
+    plt.ylabel("Amplitude")
+    plt.xlabel("Time (s)")
+    plt.show()
+
+
+def plot_delayed_sinewave(t, f=1.0, d=0.0):
+    figure(figsize=(8, 6), dpi=80)
+    sine_vals = sinewave(t, f=5, d=0.0)
+    sine_vals_delayed = sinewave(t, f=5, d=d)
+    plt.plot(t, sine_vals)
+    plt.plot(t, sine_vals_delayed, '--', c="cornflowerblue")
+    plt.ylabel("Amplitude")
+    plt.xlabel("Time (s)")
+    plt.show()
+
+# 1d
+
+
+def gabor(t, a=1.0, sigma=1, f=1.0, phi=0.0):
+    return a * np.exp((-t**2)/(2.0 * (sigma**2))) * np.cos(2*np.pi*f*t+phi)
+
+
+def gabore(t, a=1.0, sigma=1, f=1.0):
+    return gabor(t, a, sigma, f=f, phi=0.0)
+
+
+def gaboro(t, a=1.0, sigma=1, f=1.0):
+    return gabor(t, a, sigma, f=f, phi=(np.pi/2))
+
+
+def gabor_norm(fs, sigma=1, f=1.0, phi=0.0):
+    vanish_point = math.sqrt(-math.log(0.01)*2*(sigma)**2)
+    time_val = -vanish_point
+    t = []
+    while time_val < vanish_point:
+        t.append(time_val)
+        time_val += 1/fs
+
+    gabor_vals = []
+    for t_val in t:
+        gabor_vals.append(gabor(t_val, a=1.0, sigma=sigma, f=f, phi=phi))
+    return np.linalg.norm(gabor_vals, ord=2)
+
+
+def gabore_norm(fs, sigma=1, f=1.0):
+    vanish_point = math.sqrt(-math.log(0.01)*2*(sigma)**2)
+    time_val = -vanish_point
+    t = []
+    while time_val < vanish_point:
+        t.append(time_val)
+        time_val += 1/fs
+
+    gabor_vals = []
+    for t_val in t:
+        gabor_vals.append(gabore(t_val, a=1.0, sigma=sigma, f=f))
+    return np.linalg.norm(gabor_vals, ord=2)
+
+
+def gaboro_norm(fs, sigma=1, f=1.0):
+    vanish_point = math.sqrt(-math.log(0.01)*2*(sigma)**2)
+    time_val = -vanish_point
+    t = []
+    while time_val < vanish_point:
+        t.append(time_val)
+        time_val += 1/fs
+
+    gabor_vals = []
+    for t_val in t:
+        gabor_vals.append(gaboro(t_val, a=1.0, sigma=sigma, f=f))
+    return np.linalg.norm(gabor_vals, ord=2)
+
+
+def plot_gabor(t, sigma=4.0, f=1.0, a=1.0):
+    gabore_vals = []
+    for t_val in t:
+        gabore_vals.append(gabore(t_val, sigma=sigma, f=f, a=a))
+
+    figure(figsize=(8, 6), dpi=80)
+    plt.plot(t, gabore_vals)
+    plt.ylabel("Amplitude")
+    plt.xlabel("Time (s)")
+    plt.show()
+
+# 1c
+
+
+def gammatone(t, n: int = 4, f=1.0, phi=0.0, normalize = False):
+    if(type(t) == list):
+        t = np.array(t)
+    b = 1.019*(24.7*(((4.37*f)/1000) + 1))
+    gamma_value = (t**(n-1))*np.exp(-2*np.pi*b*t)*np.cos(2*np.pi*f*t + phi)
+    if normalize:
+        return gamma_value / np.amax(np.abs(gamma_value))
+    else: 
+        return gamma_value
+
+
+def plot_gammatone(t, f=1.0, xlim=(0, 0.1)):
+    gammatone_vals = gammatone(t, f=f)
+    figure(figsize=(8, 6), dpi=80)
+    plt.plot(t, gammatone_vals)
+    plt.ylabel("Amplitude")
+    plt.xlabel("Time (s)")
+    plt.xlim(xlim)
+    plt.show()
+
+# 1a
+
+
+def localmaxima(data):
+    local_maxima_indices = []
+    for i in range(len(data)):
+        if i != 0 and i != len(data)-1:
+            if data[i-1] < data[i] and data[i] > data[i+1]:
+                local_maxima_indices.append(i)
+    return local_maxima_indices
+
+
+def plot_local_maxima(t, fn_vals, lm_t, lm_vals, xlim=(0.00, 0.1), ylabel="Amplitude"):
+    figure(figsize=(8, 6), dpi=80)
+    plt.plot(t, fn_vals)
+    plt.scatter(lm_t, lm_vals, c='#ff0000')
+    plt.ylabel(ylabel)
+    plt.xlabel("Time (s)")
+    plt.xlim((0, 0.05))
+    plt.show()
+
+# 1b
+
+
+def crossings(data, threshold, dir: str = "both"):
+    crossings_indices = []
+    for i in range(len(data)):
+        if i != 0:
+            if data[i] >= threshold and data[i-1] < threshold:
+                if dir == "both" or dir == "negpos":
+                    crossings_indices.append(i)
+            elif data[i] <= threshold and data[i-1] > threshold:
+                if dir == "both" or dir == "posneg":
+                    crossings_indices.append(i)
+    return crossings_indices
+
+
+def plot_crossings(t, fn_vals, crossing_t, crossing_vals, threshold=None):
+    figure(figsize=(8, 6), dpi=80)
+    plt.plot(t, fn_vals)
+    if threshold != None:
+        plt.hlines(threshold, xmin=0.0, xmax=0.5, colors="#ff8080")
+    plt.scatter(crossing_t, crossing_vals, c="#ff0000")
+    plt.ylabel("Amplitude")
+    plt.xlabel("Time (s)")
+    plt.show()
+
+# 2c
+
+
+def envelope(y, nblocks):
+    num_samples_per_block = math.ceil(len(y)/nblocks)
+    block_indices = [0]
+    y_lower = []
+    y_upper = []
+    max_per_block = y[0]
+    min_per_block = y[0]
+    for i in range(len(y)):
+        if i != 0 and i % num_samples_per_block == 0:
+            block_indices.append(i)
+            y_upper.append(max_per_block)
+            y_lower.append(min_per_block)
+            max_per_block = y[i]
+            min_per_block = y[i]
+        elif i == len(y) - 1:
+            y_upper.append(max_per_block)
+            y_lower.append(min_per_block)
+        else:
+            if y[i] > max_per_block:
+                max_per_block = y[i]
+            elif y[i] < min_per_block:
+                min_per_block = y[i]
+    return y_lower, y_upper, block_indices
+
+
+def plot_raw_audio(wav_data):
+    figure(figsize=(8, 6), dpi=80)
+    plt.plot(range(len(wav_data)), wav_data, c="000000")
+    plt.ylabel("Amplitude")
+    plt.xlabel("Frame Index")
+    plt.show()
+
+
+def plot_envelope(y_upper, y_lower, block_indices):
+    figure(figsize=(8, 6), dpi=80)
+    plt.plot(block_indices, y_upper, c="000000")
+    plt.plot(block_indices, y_lower, c="000000")
+    plt.ylabel("Amplitude")
+    plt.xlabel("Block Index")
+    plt.show()
